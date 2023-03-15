71c3e4a15f391e804c751a74d94965734a224b7b
Nguyễn Hải Linh <medigo.linh.nguyen@gmail.com>
2023-03-15 16:21:21 +0700

    a

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+http.listen(1505, () => {
+  console.log("listening on *:1505");
+});
