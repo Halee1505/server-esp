961e24d150b993636e7dced41a99ee89c505f3cb
Nguyễn Hải Linh <medigo.linh.nguyen@gmail.com>
2023-03-17 21:20:04 +0700

    add env

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
-const serverless = require("serverless-http");
-const express = require("express");
-const http = require("http");
-const cors = require("cors");
-const pubnub = require("./config/pubnub");
+const PubNub = require("pubnub");
 
-const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-  })
-);
-
-const server = http.createServer(app);
-
-const PORT = process.env.PORT || 2222;
-
-server.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
+const pubnub = new PubNub({
+  publishKey: process.env.PUBNUB_PUBLISH_KEY,
+  subscribeKey: process.env.PUBNUB_SUBSCRIBE_KEY,
+  userId: "halee-1505",
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello Wor121sld!");
-});
-app.get("/cc", (req, res) => {
-  res.send("cc!");
-});
-app.get("/connect", (req, res) => {
-  const channel = "connect-client";
-  const message = "Connected from React Native";
-  pubnub.publish({ channel, message }, (status, response) => {
-    if (status.error) {
-      console.error(status);
-    } else {
-      console.log("Sent message to PubNub", response);
-    }
-  });
-  res.send("Connected to PubNub");
+pubnub.subscribe({
+  channels: ["connect-server"],
 });
 
-module.exports.handler = serverless(app);
+pubnub.addListener({
+  message: function (message) {
+    console.log("New Message!!", message);
+    const channel = "connect-client";
+    const ms = "Connected from Nodejs";
+    pubnub.publish({ channel, message: ms }, (status, response) => {
+      console.log("Publishing message", status, response);
+    });
+  },
+});
