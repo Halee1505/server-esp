62d2acecdc4fb315e7b633af045b2c38316e3983
Nguyễn Hải Linh <medigo.linh.nguyen@gmail.com>
2023-03-31 15:47:03 +0700

    test vercel server

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
-// const serverless = require("serverless-http");
-// const express = require("express");
-// const http = require("http");
-// const cors = require("cors");
+const serverless = require("serverless-http");
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
 const pubnub = require("./config/pubnub");
+require("dotenv").config();
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
 
-module.exports = (req, res) => {
-  pubnub.addListener({
-    message: function (message) {
-      console.log("New Message!!", message);
-      const channel = "connect-client";
-      const ms = "Connected from Nodejs";
-      pubnub.publish({ channel, message: ms }, (status, response) => {
-        console.log("Publishing message", status, response);
-      });
-    },
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 2222;
+
+server.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    message: "Hello from serverless function!",
+    code: 200,
   });
-  const { name = "World" } = req.query;
-  res.status(200).send(`Hello ${name}!`);
-};
+});
+app.get("/open", (req, res) => {
+  const channel = "client";
+  const message = "on";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("trunk open!");
+});
+app.get("/close", (req, res) => {
+  const channel = "client";
+  const message = "off";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("trunk closed!");
+});
 
-// require("dotenv").config();
-// const app = express();
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   })
-// );
+app.get("/start", (req, res) => {
+  const channel = "client";
+  const message = "start";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("start car!");
+});
+app.get("/fail", (req, res) => {
+  const channel = "client";
+  const message = "fail";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("start car fail!");
+});
+app.get("/stop", (req, res) => {
+  const channel = "client";
+  const message = "stop";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("stop car!");
+});
 
-// const server = http.createServer(app);
-
-// const PORT = process.env.PORT || 2222;
-
-// server.listen(PORT, () => {
-//   console.log(`Server running at port ${PORT}`);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello Wor121sld!");
-// });
-// app.get("/cc", (req, res) => {
-//   res.send("cc!");
-// });
-// // app.get("/connect", (req, res) => {
-// //   const channel = "connect-client";
-// //   const message = "Connected from React Native";
-// //   pubnub.publish({ channel, message }, (status, response) => {
-// //     if (status.error) {
-// //       console.error(status);
-// //     } else {
-// //       console.log("Sent message to PubNub", response);
-// //     }
-// //   });
-// //   res.send("Connected to PubNub");
-// // });
-// module.exports = serverless(app);
+module.exports = serverless(app);
