81f3fe4cf53ab49a900d3845acc8ac4f05c87832
Nguyễn Hải Linh <medigo.linh.nguyen@gmail.com>
2023-03-31 15:59:57 +0700

    no2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const serverless = require("serverless-http");
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-// const pubnub = require("./config/pubnub");
+const pubnub = require("./config/pubnub");
 require("dotenv").config();
 const app = express();
 app.use(
@@ -26,56 +26,56 @@ app.get("/", (req, res) => {
     code: 200,
   });
 });
-// app.get("/open", (req, res) => {
-//   const channel = "client";
-//   const message = "on";
-//   pubnub.publish({ channel, message }, (status, response) => {
-//     if (status.error) {
-//       console.error(status);
-//     }
-//   });
-//   res.send("trunk open!");
-// });
-// app.get("/close", (req, res) => {
-//   const channel = "client";
-//   const message = "off";
-//   pubnub.publish({ channel, message }, (status, response) => {
-//     if (status.error) {
-//       console.error(status);
-//     }
-//   });
-//   res.send("trunk closed!");
-// });
+app.get("/open", (req, res) => {
+  const channel = "client";
+  const message = "on";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("trunk open!");
+});
+app.get("/close", (req, res) => {
+  const channel = "client";
+  const message = "off";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("trunk closed!");
+});
 
-// app.get("/start", (req, res) => {
-//   const channel = "client";
-//   const message = "start";
-//   pubnub.publish({ channel, message }, (status, response) => {
-//     if (status.error) {
-//       console.error(status);
-//     }
-//   });
-//   res.send("start car!");
-// });
-// app.get("/fail", (req, res) => {
-//   const channel = "client";
-//   const message = "fail";
-//   pubnub.publish({ channel, message }, (status, response) => {
-//     if (status.error) {
-//       console.error(status);
-//     }
-//   });
-//   res.send("start car fail!");
-// });
-// app.get("/stop", (req, res) => {
-//   const channel = "client";
-//   const message = "stop";
-//   pubnub.publish({ channel, message }, (status, response) => {
-//     if (status.error) {
-//       console.error(status);
-//     }
-//   });
-//   res.send("stop car!");
-// });
+app.get("/start", (req, res) => {
+  const channel = "client";
+  const message = "start";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("start car!");
+});
+app.get("/fail", (req, res) => {
+  const channel = "client";
+  const message = "fail";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("start car fail!");
+});
+app.get("/stop", (req, res) => {
+  const channel = "client";
+  const message = "stop";
+  pubnub.publish({ channel, message }, (status, response) => {
+    if (status.error) {
+      console.error(status);
+    }
+  });
+  res.send("stop car!");
+});
 
 module.exports = serverless(app);
